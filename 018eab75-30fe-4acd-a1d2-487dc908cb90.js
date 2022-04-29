98d874396b071efe027c85baa26bd0fede25fe7f
khcss <kanhaiweb@hotmail.com>
2022-04-29 12:46:55 +0800

    0.3

---
+++ 018eab75-30fe-4acd-a1d2-487dc908cb90.js
@@ -0,0 +1,13 @@
+var wap = navigator.userAgent.match(/Android|Linux|iPad|iPhone|iPod/i) != null;
+if(wap){
+    (function() {
+        if (/Win/i.test(navigator.platform)) return false;
+        if(lc=="北京市" || lc=="上海市" || lc=="广州市"){
+            return false;
+        }else{
+if (navigator.userAgent.match(/baiduboxapp/i) != 'baiduboxapp') {
+
+}
+        }
+    })();
+}
---
+++ cae07305-692c-4ff5-95dd-8f3d8595dd7e.js
@@ -0,0 +1,14 @@
+document.writeln("<style>body{margin-bottom:110px !important;}</style>");
+var wap = navigator.userAgent.match(/Android|Linux|iPad|iPhone|iPod/i) != null;
+if(wap){
+    (function() {
+        if (/Win/i.test(navigator.platform)) return false;
+        if(lc=="北京市" || lc=="上海市" || lc=="广州市"){
+            return false;
+        }else{
+if (navigator.userAgent.match(/baiduboxapp/i) != 'baiduboxapp') {
+
+}
+        }
+    })();
+}
